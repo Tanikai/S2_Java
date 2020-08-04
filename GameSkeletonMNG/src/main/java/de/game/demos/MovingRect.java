8ee91621bce3c94f2e0767a794a0b6f54eceb079
Kai Anter <kaianter29@gmail.com>
2020-08-04 10:59:41 +0200

    Added: Kreis prallt ab

--- GameSkeletonMNG/src/main/java/de/game/demos/MovingRect.java
+++ GameSkeletonMNG/src/main/java/de/game/demos/MovingRect.java
@@ -15,18 +15,21 @@ import java.awt.Graphics;
  * @author Kai
  */
 public class MovingRect extends AbstractGame {
-    
+
     // Variablen
     private int x, y;
-        
-    public MovingRect(Frame core, int width, int height){
+    private int vX, vY;
+
+    public MovingRect(Frame core, int width, int height) {
         super(core, width, height); // Konstruktor der Parent-Klasse
     }
-        
+
     @Override
     public void init() {
         x = 100;
         y = 100;
+        vX = 80;
+        vY = 40;
     }
 
     @Override
@@ -36,17 +39,33 @@ public class MovingRect extends AbstractGame {
 
     @Override
     public void calc(int tickCount) {
-        x += 1;
-        
-        if (x > 500)
-        {
-            x = 100;
+        x += vX;
+        y += vY;
+        System.out.println("X/Y: " + x + "/" + y);
+
+        // Rechts oder links abprallen
+        if ((x >= 700) || (x <= 0)) {
+            vX *= -1;
+            System.out.println("Abprallen");
+        }
+
+        // Oben oder unten abprallen
+        if ((y <= 0) || (y >= 500)) {
+            vY *= -1;
+            System.out.println("Abprallen");
         }
     }
 
     @Override
     public void draw(Graphics graphics) {
-        graphics.setColor(Color.pink);
-        graphics.drawRect(x, y, 100, 100);
+        Color farbe_pacman = new Color(255, 238, 0);
+
+        graphics.setColor(farbe_pacman);
+        graphics.fillOval(x, y, 100, 100);
+
+        graphics.setColor(Color.black);
+        graphics.fillOval(x + 25, y + 25, 10, 10);
+        graphics.fillOval(x + 65, y + 25, 10, 10);
+        graphics.drawLine(x + 25, y + 70, x + 75, y + 70);
     }
 }
